4266eed15f13d97215baf05c29bdec5721fd99c8
Mark Fairhurst <mark@fairhursts.net>
2024-11-26 14:44:45 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+/**
+ * Create a simple TODO application with the following features:
+ *  - Add a new TODO item.
+ *  - Mark a TODO item as completed.
+ *  - Delete a TODO item.
+ *  - Filter TODO items by their status (all, completed, incomplete).
+ */
+
+function App() {
+  return (
+    <main>
+      <h1>Todo List</h1>
+    </main>
+  );
+}
+
+export default App;
